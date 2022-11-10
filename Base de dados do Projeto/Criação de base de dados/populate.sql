f8b1223f92103974862a8b8573ee8cef150c5f06
Mariana Gonçalves <marii03.goncalves@gmail.com>
2022-11-10 14:20:56 +0000

    add dicionario

---
+++ Base de dados do Projeto/Criação de base de dados/populate.sql
@@ -0,0 +1,40 @@
+/* mudar nome das equipas */
+insert into tags (tag_name) values ('Ferrari'); 
+insert into tags (tag_name) values ('Ferrari');
+insert into tags (tag_name) values ('Ferrari');
+insert into tags (tag_name) values ('Ferrari');
+insert into tags (tag_name) values ('Ferrari');
+insert into tags (tag_name) values ('Ferrari');
+insert into tags (tag_name) values ('Ferrari');
+insert into tags (tag_name) values ('Ferrari');
+insert into tags (tag_name) values ('Ferrari');
+insert into tags (tag_name) values ('Ferrari');
+
+/* insert into tu and tg ?? */
+
+insert into users (use_name, use_acc_age, use_eq_tag, use_pil_tag) values ('Francisco Costa','','1','');
+insert into users (use_name, use_acc_age, use_eq_tag, use_pil_tag) values ('Joana Santos','','4','');
+insert into users (use_name, use_acc_age, use_eq_tag, use_pil_tag) values ('Rafael Montes','','1','');
+insert into users (use_name, use_acc_age, use_eq_tag, use_pil_tag) values ('Andreia André','','7','');
+insert into users (use_name, use_acc_age, use_eq_tag, use_pil_tag) values ('Álvaro Lopes','','7','');
+
+/*alterar os nomes das equipas e ids */
+insert into grupos (gru_name, gru_desc, gru_eq_tag) values ('Grupo Ferrari','Grupo que junta pessoas com interesse na equipa de f1 Ferrari.','1');
+insert into grupos (gru_name, gru_desc, gru_eq_tag) values ('Grupo Ferrari','Grupo que junta pessoas com interesse na equipa de f1 Ferrari.','1');
+insert into grupos (gru_name, gru_desc, gru_eq_tag) values ('Grupo Ferrari','Grupo que junta pessoas com interesse na equipa de f1 Ferrari.','1');
+insert into grupos (gru_name, gru_desc, gru_eq_tag) values ('Grupo Ferrari','Grupo que junta pessoas com interesse na equipa de f1 Ferrari.','1');
+insert into grupos (gru_name, gru_desc, gru_eq_tag) values ('Grupo Ferrari','Grupo que junta pessoas com interesse na equipa de f1 Ferrari.','1');
+insert into grupos (gru_name, gru_desc, gru_eq_tag) values ('Grupo Ferrari','Grupo que junta pessoas com interesse na equipa de f1 Ferrari.','1');
+insert into grupos (gru_name, gru_desc, gru_eq_tag) values ('Grupo Ferrari','Grupo que junta pessoas com interesse na equipa de f1 Ferrari.','1');
+insert into grupos (gru_name, gru_desc, gru_eq_tag) values ('Grupo Ferrari','Grupo que junta pessoas com interesse na equipa de f1 Ferrari.','1');
+insert into grupos (gru_name, gru_desc, gru_eq_tag) values ('Grupo Ferrari','Grupo que junta pessoas com interesse na equipa de f1 Ferrari.','1');
+insert into grupos (gru_name, gru_desc, gru_eq_tag) values ('Grupo Ferrari','Grupo que junta pessoas com interesse na equipa de f1 Ferrari.','1');
+
+insert into estado (est_name) values ('Vai ao evento.');
+insert into estado (est_name) values ('Vai chegar atrasado.');
+insert into estado (est_name) values ('Não vai ao evento.');
+insert into estado (est_name) values ('Cancelou.');
+
+
+
+
