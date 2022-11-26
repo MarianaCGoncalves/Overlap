9ee54f0353d6829ba27463002e4d28143145ac8e
MarianaCGoncalves <99985414+MarianaCGoncalves@users.noreply.github.com>
2022-11-26 18:28:20 +0000

    Update populate.sql

--- Base de dados do Projeto/Criação de base de dados/populate.sql
+++ Base de dados do Projeto/Criação de base de dados/populate.sql
@@ -9,15 +9,15 @@ insert into tags (tag_name) values ('Haas'); -- 8
 insert into tags (tag_name) values ('Alphatauri'); -- 9
 insert into tags (tag_name) values ('Williams'); -- 10
 
-insert into usertag (tu_tag_id, tu_use_id) values (1,1); -- Gustavo Costa - Ferrari, RedBull
-insert into usertag (tu_tag_id, tu_use_id) values (2,6); -- Francisco Andrade - Alfa Romeo
-insert into usertag (tu_tag_id, tu_use_id) values (3,8); -- Joana Pinto - Alpine
-insert into usertag (tu_tag_id, tu_use_id) values (4,1); -- Manuel Gusmão - Ferrari
-insert into usertag (tu_tag_id, tu_use_id) values (5,6); -- Pablo Diaz - Alfa Romeo, RedBull
-insert into usertag (tu_tag_id, tu_use_id) values (6,5); -- Maria Pereira - Alpine
-insert into usertag (tu_tag_id, tu_use_id) values (7,5); -- José Almeida - Alpine
-insert into usertag (tu_tag_id, tu_use_id) values (8,3); -- Frederico Pinheiro - RedBull, Ferrari
-insert into usertag (tu_tag_id, tu_use_id) values (9,3); -- Júlia Pinheiro - RedBull, Ferrari
+insert into usertag (tu_tag_id, tu_use_id) values (1,1); -- Gustavo Costa - Ferrari
+insert into usertag (tu_tag_id, tu_use_id) values (6,3); -- Francisco Andrade - Alfa Romeo
+insert into usertag (tu_tag_id, tu_use_id) values (8,2); -- Joana Pinto - Alpine
+insert into usertag (tu_tag_id, tu_use_id) values (1,4); -- Manuel Gusmão - Ferrari
+insert into usertag (tu_tag_id, tu_use_id) values (6,5); -- Pablo Diaz - Alfa Romeo
+insert into usertag (tu_tag_id, tu_use_id) values (5,6); -- Maria Pereira - Alpine
+insert into usertag (tu_tag_id, tu_use_id) values (5,7); -- José Almeida - Alpine
+insert into usertag (tu_tag_id, tu_use_id) values (3,8); -- Frederico Pinheiro - RedBull
+insert into usertag (tu_tag_id, tu_use_id) values (3,9); -- Júlia Pinheiro - RedBull
 
 insert into grouptag (tg_tag_id, tg_gru_id) values (1,1); -- Ferrari - Grupo Ferrari
 insert into grouptag (tg_tag_id, tg_gru_id) values (2,2); -- Mercedes - Grupo Mercedes
@@ -30,40 +30,46 @@ insert into grouptag (tg_tag_id, tg_gru_id) values (8,8); -- Haas - Grupo Haas
 insert into grouptag (tg_tag_id, tg_gru_id) values (9,9); -- Alphatauri - Grupo Alphatauri
 insert into grouptag (tg_tag_id, tg_gru_id) values (10,10); -- Williams - Grupo Williams
 
-insert into user (use_name, use_acc_age, use_pass, use_mail) values ('Gustavo Costa', to_date('2022.05.06','YYYY.MM.DD'),'123','gcosta@gmail.com'); -- Criação do utilizador Gustavo Costa
-insert into user (use_name, use_acc_age, use_pass, use_mail) values ('Joana Pinto', to_date('2022.05.07','YYYY.MM.DD'),'123','joanapinto@gmail.com'); -- Criação do utilizador Joana Pinto
-insert into user (use_name, use_acc_age, use_pass, use_mail) values ('Francisco Andrade', to_date('2022.01.10','YYYY.MM.DD'),'123','frandrade@gmail.com'); -- Criação do utilizador Francisco Andrade
-insert into user (use_name, use_acc_age, use_pass, use_mail) values ('Manuel Gusmão', to_date('2022.02.26','YYYY.MM.DD'),'123','MG@gmail.com'); -- Criação do utilizador Manuel Gusmão
-insert into user (use_name, use_acc_age, use_pass, use_mail) values ('Pablo Diaz', to_date('2022.07.06','YYYY.MM.DD'),'123','PDiaz@gmail.com'); -- Criação do utilizador Pablo Diaz
-insert into user (use_name, use_acc_age, use_pass, use_mail) values ('Maria Pereira', to_date('2022.10.06','YYYY.MM.DD'),'123','maripe@gmail.com'); -- Criação do utilizador Maria Pereira
-insert into user (use_name, use_acc_age, use_pass, use_mail) values ('José Almeida', to_date('2022.05.06','YYYY.MM.DD'),'123','JAlmeida@gmail.com'); -- Criação do utilizador José Almeida
-insert into user (use_name, use_acc_age, use_pass, use_mail) values ('Frederico Pinheiro', to_date('2022.09.05','YYYY.MM.DD'),'123','pinheiro@gmail.com'); -- Criação do utilizador Frederico Pinheiro
-insert into user (use_name, use_acc_age, use_pass, use_mail) values ('Júlia Pinheiro', to_date('2022.08.03','YYYY.MM.DD'),'123','juliapinheiro@gmail.com'); -- Criação do utilizador Júlia Pinheiro
+insert into user (use_name, use_acc_age, use_pass, use_mail) values ('Gustavo Costa', to_date('2022.05.06','YYYY.MM.DD'),'123','gcosta@gmail.com'); -- Criação do utilizador Gustavo Costa.
+insert into user (use_name, use_acc_age, use_pass, use_mail) values ('Joana Pinto', to_date('2022.05.07','YYYY.MM.DD'),'123','joanapinto@gmail.com'); -- Criação do utilizador Joana Pinto.
+insert into user (use_name, use_acc_age, use_pass, use_mail) values ('Francisco Andrade', to_date('2022.01.10','YYYY.MM.DD'),'123','frandrade@gmail.com'); -- Criação do utilizador Francisco Andrade.
+insert into user (use_name, use_acc_age, use_pass, use_mail) values ('Manuel Gusmão', to_date('2022.02.26','YYYY.MM.DD'),'123','MG@gmail.com'); -- Criação do utilizador Manuel Gusmão.
+insert into user (use_name, use_acc_age, use_pass, use_mail) values ('Pablo Diaz', to_date('2022.07.06','YYYY.MM.DD'),'123','PDiaz@gmail.com'); -- Criação do utilizador Pablo Diaz.
+insert into user (use_name, use_acc_age, use_pass, use_mail) values ('Maria Pereira', to_date('2022.10.06','YYYY.MM.DD'),'123','maripe@gmail.com'); -- Criação do utilizador Maria Pereira.
+insert into user (use_name, use_acc_age, use_pass, use_mail) values ('José Almeida', to_date('2022.05.06','YYYY.MM.DD'),'123','JAlmeida@gmail.com'); -- Criação do utilizador José Almeida.
+insert into user (use_name, use_acc_age, use_pass, use_mail) values ('Frederico Pinheiro', to_date('2022.09.05','YYYY.MM.DD'),'123','pinheiro@gmail.com'); -- Criação do utilizador Frederico Pinheiro.
+insert into user (use_name, use_acc_age, use_pass, use_mail) values ('Júlia Pinheiro', to_date('2022.08.03','YYYY.MM.DD'),'123','juliapinheiro@gmail.com'); -- Criação do utilizador Júlia Pinheiro.
 
-insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Ferrari','Grupo que junta pessoas com interesse na equipa de f1 Ferrari.','0');
-insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Mercedes','Grupo que junta pessoas com interesse na equipa de f1 Mercedes.','0');
-insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo RedBull','Grupo que junta pessoas com interesse na equipa de f1 RedBull.','0');
-insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Mclaren ','Grupo que junta pessoas com interesse na equipa de f1 Mclaren.','0');
-insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Alpine','Grupo que junta pessoas com interesse na equipa de f1 Alpine.','0');
-insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Alfa Romeo','Grupo que junta pessoas com interesse na equipa de f1 Alfa Romeo.','0');
-insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Aston Martin','Grupo que junta pessoas com interesse na equipa de f1 Aston Martin.','0');
-insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Haas','Grupo que junta pessoas com interesse na equipa de f1 Haas.','0');
-insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Alphatauri','Grupo que junta pessoas com interesse na equipa de f1 Alphatauri.','0');
-insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Williams','Grupo que junta pessoas com interesse na equipa de f1 Williams.','0');
+insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Ferrari','Grupo que junta pessoas com interesse na equipa de f1 Ferrari.','0'); -- Criação do grupo Ferrari.
+insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Mercedes','Grupo que junta pessoas com interesse na equipa de f1 Mercedes.','0'); -- Criação do grupo Mercedes.
+insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo RedBull','Grupo que junta pessoas com interesse na equipa de f1 RedBull.','0'); -- Criação do grupo RedBull.
+insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Mclaren ','Grupo que junta pessoas com interesse na equipa de f1 Mclaren.','0'); -- Criação do grupo Mclaren.
+insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Alpine','Grupo que junta pessoas com interesse na equipa de f1 Alpine.','0'); -- Criação do grupo Alpine.
+insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Alfa Romeo','Grupo que junta pessoas com interesse na equipa de f1 Alfa Romeo.','0'); -- Criação do grupo Alfa Romeo.
+insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Aston Martin','Grupo que junta pessoas com interesse na equipa de f1 Aston Martin.','0'); -- Criação do grupo Aston Martin.
+insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Haas','Grupo que junta pessoas com interesse na equipa de f1 Haas.','0'); -- Criação do grupo Haas.
+insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Alphatauri','Grupo que junta pessoas com interesse na equipa de f1 Alphatauri.','0'); -- Criação do grupo Alphatauri.
+insert into grupos (gru_name, gru_desc, gru_create_age, gru_last_post_age, gru_at) values ('Grupo Williams','Grupo que junta pessoas com interesse na equipa de f1 Williams.','0'); -- Criação do grupo Williams.
 
-insert into usergroup (ug_use_id, ug_gru_id) values (1,1); -- Gustavo Costa - Grupo Ferrari
-insert into usergroup (ug_use_id, ug_gru_id) values (2,6); -- Francisco Andrade - Grupo Alfa Romeo
-insert into usergroup (ug_use_id, ug_gru_id) values (3,8); -- Joana Pinto - Alpine
-insert into usergroup (ug_use_id, ug_gru_id) values (4,1); -- Manuel Gusmão - Ferrari
-insert into usergroup (ug_use_id, ug_gru_id) values (5,6); -- Pablo Diaz - Alfa Romeo
-insert into usergroup (ug_use_id, ug_gru_id) values (6,5); -- Maria Pereira - Alpine
-insert into usergroup (ug_use_id, ug_gru_id) values (7,5); -- José Almeida - Alpine
-insert into usergroup (ug_use_id, ug_gru_id) values (8,3); -- Frederico Pinheiro - RedBull
-insert into usergroup (ug_use_id, ug_gru_id) values (9,3); -- Júlia Pinheiro - RedBull
+insert into usergroup (ug_use_id, ug_gru_id) values (1,1); -- Gustavo Costa - Grupo Ferrari.
+insert into usergroup (ug_use_id, ug_gru_id) values (2,6); -- Francisco Andrade - Grupo Alfa Romeo.
+insert into usergroup (ug_use_id, ug_gru_id) values (3,8); -- Joana Pinto - Alpine.
+insert into usergroup (ug_use_id, ug_gru_id) values (4,1); -- Manuel Gusmão - Ferrari.
+insert into usergroup (ug_use_id, ug_gru_id) values (5,6); -- Pablo Diaz - Alfa Romeo.
+insert into usergroup (ug_use_id, ug_gru_id) values (6,5); -- Maria Pereira - Alpine.
+insert into usergroup (ug_use_id, ug_gru_id) values (7,5); -- José Almeida - Alpine.
+insert into usergroup (ug_use_id, ug_gru_id) values (8,3); -- Frederico Pinheiro - RedBull.
+insert into usergroup (ug_use_id, ug_gru_id) values (9,3); -- Júlia Pinheiro - RedBull.
 
-insert into event (eve_ug_id, eve_dt, eve_ts, eve_evt_id) values (1, to_date('2022.09.22','YYYY.MM.DD'), , ); --Evento no grupo da ferrari.
+insert into event (eve_ug_id, eve_dt, eve_ts, eve_evt_id) values (4, to_date('2022.09.22','YYYY.MM.DD'), TO_TIMESTAMP ('10-Sep-02 13:23:10.123000', 'DD-Mon-RR HH24:MI:SS.FF'),2); --Evento no grupo da Ferrari criado pelo Manuel Gusmão.
+insert into event (eve_ug_id, eve_dt, eve_ts, eve_evt_id) values (6, to_date('2022.09.25','YYYY.MM.DD'), TO_TIMESTAMP ('25-Sep-22 14:10:10.123000', 'DD-Mon-RR HH24:MI:SS.FF'),3); --Evento no grupo da Alpine criado pelo José Almeida.
+insert into event (eve_ug_id, eve_dt, eve_ts, eve_evt_id) values (5, to_date('2022.10.10','YYYY.MM.DD'), TO_TIMESTAMP ('10-Out-22 14:10:10.123000', 'DD-Mon-RR HH24:MI:SS.FF'),2); --Evento no grupo da Alfa Romeo criado pelo Pablo Diaz.
 
-insert into message (mes_ug_id, mes_ts, mes_text) values (1, ,'Alguém quer almoçar para depois ir ver a corrida?'); -- Manuel pergunta no grupo.
+insert into tipoevento (evt_name) values ('WatchParty Online');
+insert into tipoevento (evt_name) values ('WatchParty');
+insert into tipoevento (evt_name) values ('Encontro Social');
+
+insert into message (mes_ug_id, mes_ts, mes_text) values (1, TO_TIMESTAMP ('22-Sep-22 14:10:10.123000', 'DD-Mon-RR HH24:MI:SS.FF'),'Alguém quer almoçar para depois ir ver a corrida?'); -- Manuel pergunta no grupo.
 
 insert into comment (comm_mes_id, comm_ug_id, comm_mes_text) values (1,1,'Eu quero! Queres combinar algum sitio para almoçar?'); -- Gustavo responde à mensagem do Manuel. (No grupo da Ferrari).
 
@@ -71,23 +77,37 @@ insert into status (st_name) values ('Vai ao evento.');
 insert into status (st_name) values ('Não vai ao evento.');
 insert into status (st_name) values ('Cancelou.');
 
-insert into usergroupevent (uge_ug_id, uge_eve_id) values (4,1); --Manuel Gusmão criou o primeiro evento do grupo da Ferrari
+insert into usergroupevent (uge_ug_id, uge_eve_id) values (4,1); -- Manuel Gusmão participa no evento da Ferrari.
+insert into usergroupevent (uge_ug_id, uge_eve_id) values (1,1); -- Gustavo Costa participa no evento da Ferrari.
+insert into usergroupevent (uge_ug_id, uge_eve_id) values (7,2); -- José Almeida participa no evento do grupo da Alpine.
+insert into usergroupevent (uge_ug_id, uge_eve_id) values (6,2); -- Maria Pereira participa no evento do grupo da Alpine.
+insert into usergroupevent (uge_ug_id, uge_eve_id) values (3,2); -- Joana Pinto participa no evento do grupo da Alpine.
+insert into usergroupevent (uge_ug_id, uge_eve_id) values (5,3); -- Pablo Diaz participa no evento do grupo da Alfa Romeo.
+insert into usergroupevent (uge_ug_id, uge_eve_id) values (2,3); -- Francisco Andrade participa no evento do grupo da Alfa Romeo.
 
-insert into eveonline (eve_on_name, eve_on_desc, eve_on_dt, eve_on_eve_id) values ('WatchParty!', 'WatchParty no grupo de discord! Link do grupo: ',to_date('2022.09.22','YYYY.MM.DD'), );
+insert into eveonline (eve_on_name, eve_on_desc, eve_on_dt, eve_on_eve_id) values ('WatchParty!', 'WatchParty no grupo de discord! Link do grupo: ',to_date('2022.10.10','YYYY.MM.DD'),3); -- Evento Online da Alfa Romeo.
 
-insert into evefisico (eve_phy_name, eve_phy_desc, eve_phy_dt, eve_phy_eve_id) values ('Assistir a corrida em Santos!', 'Assistir a corrida em Santos que começa às 15 horas. Vamos nos encontrar ao pé da Taska da Txika às 14:45h!', to_date('2022.07.06','YYYY.MM.DD'),1);
+insert into evefisico (eve_phy_name, eve_phy_desc, eve_phy_dt, eve_phy_eve_id) values ('Assistir a corrida em Santos!', 'Assistir a corrida em Santos que começa às 15 horas. Vamos nos encontrar ao pé da Taska da Txika às 14:45h!', to_date('2022.09.22','YYYY.MM.DD'),1); --Evento fisico do grupo Ferrari.
+insert into evefisico (eve_phy_name, eve_phy_desc, eve_phy_dt, eve_phy_eve_id) values ('Beber um café', 'Vamos beber um café no Patio café, para falar sobre as estrategias da Alpine.', to_date('2022.07.25','YYYY.MM.DD'),2); --Evento fisico do grupo Alpine.
 
-insert into localizacao (local_name, local_point) values ('Santos', 38.707093, -9.152712); -- utilizando a variavel point do PostGIS.
-insert into localizacao (local_name, local_point) values ('Belém', 38.697414, -9.199386);
-insert into localizacao (local_name, local_point) values ('Algés', 38.700895, -9.230866);
-insert into localizacao (local_name, local_point) values ('Oeiras', 38.696764, -9.314625); 
-insert into localizacao (local_name, local_point) values ('Caxias', 38.702858, -9.269295);
+-- utilizando a variavel point do PostGIS.
 
-insert into evephylocat (eve_phylocat_evephy_id, eve_phylocat_locat_id) values ();
+insert into localizacao (local_name, local_point) values ('Santos', 38.707093 -9.152712); -- Localização Santos.
+insert into localizacao (local_name, local_point) values ('Belém', 38.697414 -9.199386);-- Localização Belém.
+insert into localizacao (local_name, local_point) values ('Algés', 38.700895 -9.230866);-- Localização Algés.
+insert into localizacao (local_name, local_point) values ('Oeiras', 38.696764 -9.314625); -- Localização Oeiras.
+insert into localizacao (local_name, local_point) values ('Caxias', 38.702858 -9.269295);-- Localização Caxias.
 
--- tipo de evento : enumeracao?
+insert into evephylocat (eve_phylocat_evephy_id, eve_phylocat_locat_id) values (1,1); --Primeiro evento fisico da ferrari está localizado em Santos.
+insert into evephylocat (eve_phylocat_evephy_id, eve_phylocat_locat_id) values (2,3); --Primeiro evento fisico da Alpine está localizado em Algés.
 
-insert into ugestatus (ugest_uge_id, ugest_est_id) values ();
+insert into ugestatus (ugest_uge_id, ugest_est_id) values (1,1); -- Manuel Gusmão vai ao evento.
+insert into ugestatus (ugest_uge_id, ugest_est_id) values (2,1); -- Gustavo Costa vai ao evento.
+insert into ugestatus (ugest_uge_id, ugest_est_id) values (3,1); -- José Almeida vai ao evento.
+insert into ugestatus (ugest_uge_id, ugest_est_id) values (4,1); -- Maria Pereira vai ao evento.
+insert into ugestatus (ugest_uge_id, ugest_est_id) values (5,1); -- Joana Pinto vai ao evento.
+insert into ugestatus (ugest_uge_id, ugest_est_id) values (6,1); -- Pablo Diaz vai ao evento.
+insert into ugestatus (ugest_uge_id, ugest_est_id) values (7,1); -- Francisco Andrade vai ao evento.
 
 insert into follows (fol_use_idA, fol_use_idB) values (1,4); -- Gustavo segue o Manuel.
 insert into follows (fol_use_idA, fol_use_idB) values (4,1); -- Manuel segue o Gustavo.
