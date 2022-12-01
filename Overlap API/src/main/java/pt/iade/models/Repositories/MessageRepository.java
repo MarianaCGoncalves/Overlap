e110dedb6a11b1a34ae60f58461e393620221778
MarianaCGoncalves <99985414+MarianaCGoncalves@users.noreply.github.com>
2022-12-01 20:58:05 +0000

    Update MessageRepository.java

--- Overlap API/src/main/java/pt/iade/models/Repositories/MessageRepository.java
+++ Overlap API/src/main/java/pt/iade/models/Repositories/MessageRepository.java
@@ -10,6 +10,6 @@ import pt.iade.models.Message;
  
 public interface MessageRepository extends CrudRepository<Message,Integer> {
 
-    @Query(value = "Select * from message left join usergroup ON ug_id = mes_ug_id where ug_gru_id = :gid ORDER BY mes_ts DESC LIMIT 1", nativeQuery = true)
+    @Query(value = "Select * from mensagem left join usergroup ON ug_id = mes_ug_id where ug_gru_id = :gid ORDER BY mes_ts DESC LIMIT 1", nativeQuery = true)
     Optional<Message> getLastMessage(@Param("gid") int gid);
  } 
