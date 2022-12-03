fe140b07a12ffa1f333e020f0eabe85dc0f033ff
MarianaCGoncalves <99985414+MarianaCGoncalves@users.noreply.github.com>
2022-12-03 15:59:20 +0000

    Update populate.sql

--- Base de dados do Projeto/Criação de base de dados/populate.sql
+++ Base de dados do Projeto/Criação de base de dados/populate.sql
@@ -65,9 +65,9 @@ insert into mensagem (mes_ug_id, mes_date, mes_text, mes_use_id) values (1, TO_T
 
 insert into comment (comm_mes_id, comm_ug_id, comm_mes_text) values (1,1,'Eu quero! Queres combinar algum sitio para almoçar?'); -- Gustavo responde à mensagem do Manuel. (No grupo da Ferrari).
 
-insert into eventos (eve_ug_id, eve_name, eve_desc, eve_evt_id, eve_date) values (4, 'Assistir a corrida em Santos!', 'Assistir a corrida em Santos que começa às 15 horas. Vamos nos encontrar ao pé da Taska da Txika às 14:45h!',3, TO_TIMESTAMP ('2022-09-10 13:23:25', 'YYYY-MM-DD HH24:MI:SS')); --Evento no grupo da Ferrari criado pelo Manuel Gusmão.
-insert into eventos (eve_ug_id, eve_name, eve_desc, eve_evt_id, eve_date) values (6,'WatchParty','WatchParty no grupo de discord! Link do grupo:',3, TO_TIMESTAMP ('2022-10-10 14:10:25', 'YYYY-MM-DD HH24:MI:SS')); --Evento no grupo da Alfa Romeo criado pelo Pablo Diaz. )
-insert into eventos (eve_ug_id, eve_name, eve_desc, eve_evt_id, eve_date) values (5,'Beber um café', 'Vamos beber um café no Patio café, para falar sobre as estrategias da Alpine.',2, TO_TIMESTAMP ('2022-09-25 14:30:25', 'YYYY-MM-DD HH24:MI:SS')); --Evento no grupo da Alpine criado pelo José Almeida )
+insert into eventos (eve_ug_id, eve_name, eve_desc, eve_evt_id, eve_ts) values (4, 'Assistir a corrida em Santos!', 'Assistir a corrida em Santos que começa às 15 horas. Vamos nos encontrar ao pé da Taska da Txika às 14:45h!',3, TO_TIMESTAMP ('2022-09-10 13:23:25', 'YYYY-MM-DD HH24:MI:SS')); --Evento no grupo da Ferrari criado pelo Manuel Gusmão.
+insert into eventos (eve_ug_id, eve_name, eve_desc, eve_evt_id, eve_ts) values (6,'WatchParty','WatchParty no grupo de discord! Link do grupo:',3, TO_TIMESTAMP ('2022-10-10 14:10:25', 'YYYY-MM-DD HH24:MI:SS')); --Evento no grupo da Alfa Romeo criado pelo Pablo Diaz. )
+insert into eventos (eve_ug_id, eve_name, eve_desc, eve_evt_id, eve_ts) values (5,'Beber um café', 'Vamos beber um café no Patio café, para falar sobre as estrategias da Alpine.',2, TO_TIMESTAMP ('2022-09-25 14:30:25', 'YYYY-MM-DD HH24:MI:SS')); --Evento no grupo da Alpine criado pelo José Almeida )
 
 insert into tipoevento (evt_name) values ('WatchParty Online');
 insert into tipoevento (evt_name) values ('WatchParty');
